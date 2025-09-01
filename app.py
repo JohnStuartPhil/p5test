b77313b30cf3bb0d54399cd51b8b1b1e2cb20cf5
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-09-01 19:24:58 +0000

    Add initial page 4 streamlit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app_pages.multi_page import MultiPage
 from app_pages.page1 import page1_body
 from app_pages.page2 import page2_body
 from app_pages.page3 import page3_body
+from app_pages.page4 import page4_body
 
 # Create an instance
 app = MultiPage(app_name="Iowa House Prices")
@@ -12,5 +13,6 @@ app = MultiPage(app_name="Iowa House Prices")
 app.app_page("Page 1", page1_body)
 app.app_page("Page 2", page2_body)
 app.app_page("Page 3", page3_body)
+app.app_page("Page 4", page4_body)
 
 app.run()
---
+++ app_pages/page4.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def page4_body():
+    """ Page 4 contents"""
+    st.write("Page 4 contents here")
